aa4740fbf7a9f58c3163c7e7396cfd618b9eeab9
Ayush200230 <humbleayush30@gmail.com>
2024-08-02 18:55:11 +0530

    cors updated

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 const user = {
     full_name: 'John_Doe',
